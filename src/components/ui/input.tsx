86b04002382a9370303113c61e1cc7d8e9c566e9
Maksim C <jstars.tech@gmail.com>
2025-12-01 20:38:32 +0300

    Feat: add customClassName to set completely custom classes for the component

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -4,19 +4,17 @@ import * as React from 'react'
 const Input: React.FC<
   {
     ref?: React.Ref<HTMLInputElement>
+    customClassName?: string | undefined
   } & React.InputHTMLAttributes<HTMLInputElement>
-> = ({ type, className, ref, ...props }) => {
-  return (
-    <input
-      className={cn(
+> = ({ type, className, customClassName, ref, ...props }) => {
+  const inputClassName = customClassName
+    ? customClassName
+    : cn(
         'flex h-10 w-full rounded border border-border bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
         className,
-      )}
-      ref={ref}
-      type={type}
-      {...props}
-    />
-  )
+      )
+
+  return <input className={inputClassName} ref={ref} type={type} {...props} />
 }
 
 export { Input }
