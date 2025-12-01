f849cd41679f58db3cbbf260c4497dbcebc3ed37
Maksim C <jstars.tech@gmail.com>
2025-12-01 11:57:59 +0300

    Feat: add display blog posts

--- src/components/CollectionArchive/index.tsx
+++ src/components/CollectionArchive/index.tsx
@@ -5,16 +5,26 @@ import { Card, CardPostData } from '@/components/Card'
 
 export type Props = {
   posts: CardPostData[]
+  relationTo?: string
 }
 
 export const CollectionArchive: React.FC<Props> = (props) => {
-  const { posts } = props
+  const { posts, relationTo = 'posts' } = props
 
   return (
     <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
       {posts?.map((result, index) => {
         if (typeof result === 'object' && result !== null) {
-          return <Card className="h-full" doc={result} relationTo="posts" showCategories />
+          return (
+            <Card
+              className="h-full"
+              doc={result}
+              relationTo={relationTo}
+              key={index}
+              title=""
+              showCategories
+            />
+          )
         }
 
         return null
